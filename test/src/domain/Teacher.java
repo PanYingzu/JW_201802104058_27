d429fee654b15a4a6e92829b425f05649e4dc6f0
Pan Yingzu <1013232282@qq.com>
2019-11-27 21:49:47 +0800

    提交到本地仓库

---
+++ test/src/domain/Teacher.java
@@ -0,0 +1,79 @@
+//201802104058潘英祖
+package domain;
+
+import java.io.Serializable;
+
+public final class Teacher implements Comparable<Teacher>,Serializable {
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private ProfTitle profTitle;
+	private Degree degree;
+	private Department department;
+
+	public Teacher(Integer id,
+				   String name,
+				   ProfTitle profTitle,
+				   Degree degree,
+                   Department department) {
+		this(name, profTitle, degree, department);
+		this.id = id;
+
+	}
+	public Teacher(
+				   String name,
+				   ProfTitle profTitle,
+				   Degree degree,
+				   Department department) {
+		this.name = name;
+		this.profTitle = profTitle;
+		this.degree = degree;
+		this.department = department;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ProfTitle getProfTitle() {
+		return this.profTitle;
+	}
+
+	public void setProfTitle(ProfTitle profTitle) {
+		this.profTitle = profTitle;
+	}
+
+	public Degree getDegree() {
+		return degree;
+	}
+
+	public void setDegree(Degree degree) {
+		this.degree = degree;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int compareTo(Teacher o) {
+		// TODO Auto-generated method stub
+		return this.id-o.getId();
+	}
+}
